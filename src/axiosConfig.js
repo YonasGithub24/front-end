8047b2cab28e760d0aab5bbf73a4d90d3d29113c
YonasGithub24 <yonaa2000@gmail.com>
2024-06-26 21:22:51 -0500

    Axiois nodify

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosBase = axios.create({
-  baseURL: "http://localhost:5500/api",
+  // baseURL: "http://localhost:5500/api",
+  baseURL: "https://back-end-9au5.onrender.com/api",
 });
 
 export default axiosBase;
